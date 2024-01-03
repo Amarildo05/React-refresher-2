6c5c86e5eabdcbe54da460cf1ceea89cb05f1bff
Amarildo <amarildokola36@gmail.com>
2024-01-03 23:54:56 +0100

    useState/lifting state up

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,10 @@
 import classes from "./Post.module.css";
 
-const names = ["Amarildo", "Lido"];
-const content = ["React refresher course", "React updated course"];
-
 export default function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-  const choseContent = Math.random() > 0.5 ? content[0] : content[1];
-
   return (
     <li className={classes.post}>
-      <h1 className={classes.author}>{chosenName}</h1>
-      <h1 className={classes.body}>{choseContent}</h1>
+      <h1 className={classes.author}></h1>
+      <h1 className={classes.body}></h1>
     </li>
   );
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 export default function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post />
-        <Post />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Amarildo" body="Check out the full course!" />
       </ul>
     </>
   );
