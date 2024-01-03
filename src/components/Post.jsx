400cd1feeef8abf2c4e0ecfdb9b9a1e7adb76c66
Amarildo <amarildokola36@gmail.com>
2024-01-04 00:01:41 +0100

    little fix

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,10 @@
 import classes from "./Post.module.css";
 
-export default function Post() {
+export default function Post(props) {
   return (
     <li className={classes.post}>
-      <h1 className={classes.author}></h1>
-      <h1 className={classes.body}></h1>
+      <h1 className={classes.author}>{props.author}</h1>
+      <h1 className={classes.body}>{props.body}</h1>
     </li>
   );
 }
