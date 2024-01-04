31a66721e80b8afc336eef32a07f915488fcf570
Amarildo <amarildokola36@gmail.com>
2024-01-04 23:18:42 +0100

    passing some props/another State ... needs adj

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal";
 
 export default function PostsList() {
+  const [modalVisible, setModalVisible] = useState(true);
+
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -17,10 +24,14 @@ export default function PostsList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Amarildo" body="Check out the full course!" />
