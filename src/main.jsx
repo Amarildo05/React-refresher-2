33af85e5588724206a93ee86e27568fa069398ba
Amarildo <amarildokola36@gmail.com>
2024-01-09 23:43:07 +0100

    Routing/Navigating/Clean up code

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
+import Posts from "./routes/Posts";
 import "./index.css";
+import RootLayout from "./routes/RootLayout";
+import NewPost from "./routes/NewPost";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  // { path: "/create-post", element: <NewPost /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
